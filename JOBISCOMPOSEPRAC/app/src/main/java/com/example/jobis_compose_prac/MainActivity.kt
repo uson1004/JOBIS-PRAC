7845cec2baa793b517c025289ff7841693270901
유섭 <yoonbk2001027@gmail.com>
2024-12-16 15:28:01 +0900

    setting color, padding value

--- JOBISCOMPOSEPRAC/app/src/main/java/com/example/jobis_compose_prac/MainActivity.kt
+++ JOBISCOMPOSEPRAC/app/src/main/java/com/example/jobis_compose_prac/MainActivity.kt
@@ -4,23 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
-import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,19 +32,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.jobis_compose_prac.ui.theme.Grayscale30
+import com.example.jobis_compose_prac.ui.theme.Grayscale50
 import com.example.jobis_compose_prac.ui.theme.Grayscale60
 import com.example.jobis_compose_prac.ui.theme.JOBISCOMPOSEPRACTheme
 import com.example.jobis_compose_prac.ui.theme.Primary20
@@ -61,27 +66,39 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
-    Column(modifier) {
-        BackStackRow()
-        LoginJOBISElement()
-        InputLayoutColumns()
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        Column(modifier) {
+            BackStackRow()
+            LoginJOBISElement()
+            InputLayoutColumns()
+            ForgetButtonElement()
+        }
+
+        JOBISLoginElement(modifier.align(Alignment.BottomCenter))
     }
 }
 
 @Composable
 fun BackStackRow(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     Row(
         modifier
             .fillMaxWidth()
-            .padding(vertical = 8.dp, horizontal = 20.dp)
+            .padding(vertical = 8.dp)
     ) {
-        Icon(
-            painter = painterResource(R.drawable.arrow),
-            contentDescription = null,
-            modifier = modifier.size(28.dp)
-        )
+        Spacer(modifier = modifier.width(14.dp))
+        IconButton(onClick = {}) {
+            Icon(
+                painter = painterResource(R.drawable.arrow_back),
+                contentDescription = null,
+                modifier = modifier.size(24.dp),
+            )
+        }
     }
 }
 
@@ -92,8 +109,9 @@ fun LoginJOBISElement(
     Row(
         modifier
             .fillMaxWidth()
-            .padding(vertical = 20.dp, horizontal = 24.dp)
+            .padding(vertical = 20.dp)
     ) {
+        Spacer(modifier = modifier.width(24.dp))
         Text(
             buildAnnotatedString {
                 withStyle(style = SpanStyle(color = Primary20)) {
@@ -109,6 +127,7 @@ fun LoginJOBISElement(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InputLayoutColumns(
     modifier: Modifier = Modifier
@@ -116,12 +135,14 @@ fun InputLayoutColumns(
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
+    var showPassword by remember { mutableStateOf(false) }
+
     Column(modifier.fillMaxWidth()) {
         Column(modifier.padding(vertical = 12.dp, horizontal = 24.dp)) {
             Text(
                 text = "이메일",
                 style = TextStyle(fontSize = 14.sp),
-                modifier = modifier.padding(bottom = 4.dp)
+                modifier = modifier.padding(bottom = 8.dp)
             )
             OutlinedTextField(
                 value = email,
@@ -130,22 +151,40 @@ fun InputLayoutColumns(
                 },
                 singleLine = true,
                 placeholder = {
-                    Text(
-                        text = "example"
-                    )
-                    Text(
-                        text = "@dsm.hs.kr",
-                        textAlign = TextAlign.End
-                    )
+                    Row(modifier = modifier.fillMaxWidth()) {
+                        Text(
+                            text = "example",
+                            textAlign = TextAlign.Start,
+                            style = TextStyle(
+                                fontWeight = FontWeight.Bold
+                            )
+                        )
+                        Spacer(modifier = modifier.width(200.dp))
+                        Text(
+                            text = "@dsm.hs.kr",
+                            textAlign = TextAlign.End,
+                            style = TextStyle(
+                                fontWeight = FontWeight.Bold
+                            )
+                        )
+                    }
                 },
-                modifier = modifier.fillMaxWidth()
+                modifier = modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(12.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Grayscale30,
+                    unfocusedBorderColor = Grayscale30,
+                    containerColor = Grayscale30,
+                    focusedPlaceholderColor = Grayscale60,
+                    unfocusedPlaceholderColor = Grayscale60,
+                )
             )
         }
         Column(modifier.padding(vertical = 12.dp, horizontal = 24.dp)) {
             Text(
                 text = "비밀번호",
                 style = TextStyle(fontSize = 14.sp),
-                modifier = modifier.padding(bottom = 4.dp)
+                modifier = modifier.padding(bottom = 8.dp)
             )
             OutlinedTextField(
                 modifier = modifier.fillMaxWidth(),
@@ -155,17 +194,112 @@ fun InputLayoutColumns(
                 },
                 singleLine = true,
                 placeholder = {
-                    Text(text = "비밀번호를 입력해주세요.")
+                    Text(
+                        text = "비밀번호를 입력해주세요.",
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold
+                        ),
+                    )
                 },
+                visualTransformation =
+                if (showPassword)
+                    VisualTransformation.None
+                else
+                    PasswordVisualTransformation(),
                 trailingIcon = {
-                    IconButton(onClick = {}) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.eye_close),
-                            contentDescription = null,
-                            modifier = modifier.size(24.dp)
-                        )
+                    if (showPassword) {
+                        IconButton(onClick = { showPassword = false }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.eye_open),
+                                contentDescription = null,
+                                modifier = modifier.size(24.dp),
+                            )
+                        }
+                    } else {
+                        IconButton(onClick = { showPassword = true }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.eye_close),
+                                contentDescription = null,
+                                modifier = modifier.size(24.dp),
+                            )
+                        }
                     }
-                }
+                },
+                shape = RoundedCornerShape(12.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Grayscale30,
+                    unfocusedBorderColor = Grayscale30,
+                    containerColor = Grayscale30,
+                    focusedPlaceholderColor = Grayscale60,
+                    unfocusedPlaceholderColor = Grayscale60
+                ),
+            )
+        }
+    }
+}
+
+@Composable
+fun ForgetButtonElement(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Button(
+            onClick = {},
+            modifier = modifier
+                .padding(horizontal = 74.5.dp, vertical = 8.dp)
+                .align(Alignment.Center)
+                .height(48.dp),
+            shape = RoundedCornerShape(32.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Grayscale30,
+                contentColor = Color.Black,
+            ),
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.unlock_password),
+                contentDescription = null,
+                modifier = modifier
+                    .size(24.dp)
+                    .padding(end = 4.dp)
+            )
+            Text(text = "비밀번호를 잊으셨나요?")
+        }
+    }
+}
+
+@Composable
+fun JOBISLoginElement(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Button(
+            onClick = {},
+            modifier = modifier
+                .padding(horizontal = 24.dp, vertical = 12.dp)
+                .fillMaxWidth()
+                .height(52.dp),
+            shape = RoundedCornerShape(12.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Primary20,
+                contentColor = Color.White,
+                disabledContainerColor = Grayscale50,
+                disabledContentColor = Color.White
+            )
+        ) {
+            Text(
+                text = "로그인",
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            )
+            Icon(
+                painter = painterResource(R.drawable.arrow_right),
+                contentDescription = null,
+                modifier = modifier
+                    .padding(start = 8.dp)
+                    .size(24.dp)
             )
         }
     }
--- JOBISCOMPOSEPRAC/app/src/main/java/com/example/jobis_compose_prac/ui/theme/Color.kt
+++ JOBISCOMPOSEPRAC/app/src/main/java/com/example/jobis_compose_prac/ui/theme/Color.kt
@@ -10,5 +10,9 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val Grayscale90 = Color(0x00000000)
+val Grayscale30 = Color(0xF7F7F7F7)
+val Grayscale50 = Color(0xCCCCCCCC)
 val Grayscale60 = Color(0x7F7F7F7F)
 val Primary20 = Color(0xFF2F52FD)
+
